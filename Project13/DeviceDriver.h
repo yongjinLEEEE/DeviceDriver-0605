5a16965b2dc78bf58485529aabbca29ee16da53e
yongjin lee <vir2048@gmail.com>
2023-06-05 14:05:07 +0900

    DeivceDriver base code

---
+++ Project13/DeviceDriver.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "FlashMemoryDevice.h"
+
+class DeviceDriver {
+public:
+    DeviceDriver(FlashMemoryDevice* hardware);
+    int read(long address);
+    void write(long address, int data);
+
+protected:
+    FlashMemoryDevice* m_hardware;
+};
