d9492f1c675ac94ac0f8e84c649191c875fa2ca7
yongjin lee <vir2048@gmail.com>
2023-06-05 15:34:00 +0900

    [DD] update read funtion
    UTC0 result - pass
    
    Signed-off-by: yongjin lee <vir2048@gmail.com>

--- Project13/DeviceDriver.cpp
+++ Project13/DeviceDriver.cpp
@@ -4,8 +4,12 @@ DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware) {
 }
 
 int DeviceDriver::read(long address) {
-    // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+    int tmp;
+
+    for (int i = 0; i < 5; i++)
+        tmp = (int)(m_hardware->read(address));
+
+    return tmp;
 }
 
 void DeviceDriver::write(long address, int data) {
