2aee101905780dec8c912371b00cd0bd2d52f291
yongjin lee <vir2048@gmail.com>
2023-06-05 15:57:48 +0900

    [test] Add dest memory check UTC on write
    result - failed
    
    Signed-off-by: yongjin lee <vir2048@gmail.com>

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -44,3 +44,26 @@ TEST(TestCaseName, memCheck) {
 	DeviceDriver dd(&dev_mock);
 	dd.write(0xaa, 0xa);
 }
+
+TEST(TestCaseName, memCheckForWrite1) {
+	DevMock dev_mock;
+
+	EXPECT_CALL(dev_mock, read(_))
+		.Times(1)
+		.WillOnce(Return(0xFF));
+
+	DeviceDriver dd(&dev_mock);
+	dd.write(0xaa, 0xa);
+}
+
+TEST(TestCaseName, memCheckForWrite2) {
+	DevMock dev_mock;
+
+	EXPECT_CALL(dev_mock, read(_))
+		.Times(1)
+		.WillOnce(Return(0x2));
+
+	DeviceDriver dd(&dev_mock);
+
+	EXPECT_THROW(dd.write(0xaa, 0xa), exception);
+}
