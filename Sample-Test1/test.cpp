db74e7a93894254eaae43927aed5961b02070c61
yongjin lee <vir2048@gmail.com>
2023-06-05 15:28:23 +0900

    [test] add device mock

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,8 +1,18 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
-#include "../Project13/DeviceDriver.h"
+#include "../Project13/DeviceDriver.cpp"
+
+using namespace testing;
+class DevMock : public FlashMemoryDevice
+{
+public:
+	MOCK_METHOD(unsigned char, read, (long address), (override));
+	MOCK_METHOD(void, write, (long address, unsigned char data), (override));
+};
 
 TEST(TestCaseName, TestName) {
-	EXPECT_EQ(1, 1);
-	EXPECT_TRUE(true);
+	DevMock dev_mock;
+
+	DeviceDriver dd(&dev_mock);
+	
 }
