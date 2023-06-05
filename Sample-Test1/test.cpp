c3dea127d9216af130a31fc152e9142ecc103ef5
yongjin lee <vir2048@gmail.com>
2023-06-05 14:27:17 +0900

    update test.cpp

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,6 +1,7 @@
-#include "pch.h"
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
 TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+	EXPECT_EQ(1, 1);
+	EXPECT_TRUE(true);
 }
