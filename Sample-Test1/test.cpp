5585d697921cabec4eee20d77a0d99574e977b31
yongjin lee <vir2048@gmail.com>
2023-06-05 15:30:33 +0900

    [test] Add UTC - read5th
    result - failed
    
    Signed-off-by: yongjin lee <vir2048@gmail.com>

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -10,9 +10,12 @@ public:
 	MOCK_METHOD(void, write, (long address, unsigned char data), (override));
 };
 
-TEST(TestCaseName, TestName) {
+TEST(TestCaseName, read5th) {
 	DevMock dev_mock;
 
+	EXPECT_CALL(dev_mock, read)
+		.Times(5);
+
 	DeviceDriver dd(&dev_mock);
-	
+	dd.read(0xaaa);
 }
